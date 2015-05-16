2c621e3562b98a98fa97ac2b579dd1f01990ccd0
unknown <andrew.langford92@gmail.com>
2015-05-15 21:00:23 -0400

    attempted to clean up some code, on to booleans and boolean expressions

--- src/CodeGenerator.java
+++ src/CodeGenerator.java
@@ -111,28 +111,48 @@ public class CodeGenerator {
 	 * @return a string array containing the opcodes for the program we wish to run
 	 */
 	public String[] generateProgramCode(){
+		System.out.println("\n\n--->Beginning Code Generation\n");
+		
+		//generate code for the ast
 		generateStatementCode(ast.getRoot());
+		
 		//add a break to the end of the code.
 		addByte("00");
+		
+		//patch all temporary positions
 		backPatchStatic();
+		
+		//patch all temporary jumps
 		backPatchJumps();
+		
+		//return the program image
 		return codeTable;
 	}
 	
+	/**
+	 * Fills all the temp locations with their actual positions in memory
+	 */
 	private void backPatchStatic() {
 		for(int i =0; i < codeTable.length; i++){
+			
+			//have to check every cell for temp locations
 			StaticTable.StaticEntry entry = staticTable.getEntryFromTable(codeTable[i]);
 			if(entry!= null){
+				
+				//currentByte pointer will hold the first available address
 				int staticPlacement = currentByte + entry.getOffset();
 				String hexRepresentation = Integer.toHexString(staticPlacement);
 				if(hexRepresentation.length()<2){
 					hexRepresentation = "0" + hexRepresentation;
 				}
+				
+				//replace the temporary location with the actual location
 				codeTable[i] = hexRepresentation.toUpperCase();
+				
+				//replace XX bytes
 				codeTable[i+1] = "00";
 			}
 		}
-		// TODO Auto-generated method stub
 		
 	}
 	
@@ -144,18 +164,31 @@ public class CodeGenerator {
 	private int addStringToHeap(String stringToAdd){
 		//null terminate the string
 		addByteToHeap("00");
+		
+		//add a byte for each character
 		for(int i = stringToAdd.length()-1; i>=0; i--){
+			
+			//format it in hex
 			char characterToWrite = stringToAdd.charAt(i);
 			String hexRepresentation = String.format("%02x", (int) characterToWrite);
 		
+			//make sure its length 2
 			if(hexRepresentation.length() <2){
 				hexRepresentation = "0" + hexRepresentation;
 			}
+			
+			//add the byte to the heap
 			addByteToHeap(hexRepresentation.toUpperCase());
 		}
+		
+		//return the address
 		return heapPointer + 1;
 	}
 	
+	
+	/**
+	 * patches temporary jump locations
+	 */
 	private void backPatchJumps(){
 		
 		
@@ -169,10 +202,11 @@ public class CodeGenerator {
 	 * @return A String if an expr is being evaluated, null otherwise.
 	 * 
 	 */
-	private String generateStatementCode(Node currentAstNode){
-		
-		
+	private void generateStatementCode(Node currentAstNode){
+	
 		switch(currentAstNode.getValue()){
+		
+			//generate block code
 			case("Block"):{
 				System.out.println("Generating Code for a block");
 				handleBlock(currentAstNode);
@@ -180,6 +214,7 @@ public class CodeGenerator {
 			
 			break;
 		
+			//generate variable declaration code
 			case("VarDecl"):{
 				System.out.println("Generating Code for a Variable Declaration");
 				handleVarDecl(currentAstNode);
@@ -187,22 +222,23 @@ public class CodeGenerator {
 			
 			break;
 			
+			//generate while statement code
 			case("WhileStatement"):{
 				System.out.println("Generating Code for a While Statement");
-				//pass the while statement's block to this function 
 				handleWhileStatement(currentAstNode);
 			}
 			
 			break;
 			
+			//generate if statement code
 			case("IfStatement"):{
 				System.out.println("Generating Code for an If Statement");
-				//pass the if statmenen'ts block to this function
 				handleIfStatement(currentAstNode);
 			}
 			
 			break;
 			
+			//generate print statement code
 			case("PrintStatement") :{
 				System.out.println("Generating Code for a Print Statement");
 				handlePrintStatement(currentAstNode);
@@ -212,6 +248,8 @@ public class CodeGenerator {
 			
 			break;
 			
+			
+			//generate assignment code
 			case("AssignmentStatement") :{
 				System.out.println("Generating Code for an Assignment Statement");
 				handleAssignmentStatement(currentAstNode);
@@ -220,53 +258,62 @@ public class CodeGenerator {
 			break;
 			
 
+			//if we get here, theres some expression to evaluate
 			default :{
 				
+				//generate code for some identifier reference
 				if(currentAstNode.getValue().matches("[a-z]")){
 					System.out.println("Handling code gen for an identifier");
-					return handleIdentifier(currentAstNode, currentAstNode.getToken());
+					handleIdentifier(currentAstNode);
 	
 				}
 				
+				//generate code for some digit reference
 				if(currentAstNode.getValue().matches("[0-9]")){
 					System.out.println("Handling code gen for a digit");
-					return handleDigit(currentAstNode);
+					handleDigit(currentAstNode);
 				}
 				
+				//generate code for some boolean value reference
 				if(currentAstNode.getValue().matches("true|false")){
 					System.out.println("Handling code gen for a boolean");
-					return handleBoolVal(currentAstNode);
+					handleBoolVal(currentAstNode);
 				}
 				
+				//generate code for some string reference
 				if(currentAstNode.getValue().matches("\"([^\"]*)\"")){
 					System.out.println("Handling code gen for a string");
-					return handleCharList(currentAstNode);
+					handleCharList(currentAstNode);
 				}
 				
+				//generate code for an addition (intop) expression
 				if(currentAstNode.getValue().matches("\\+")){
 					System.out.println("Handling code gen for an integer operation");
-					return handleAdditionOperation(currentAstNode);
+					handleAdditionOperation(currentAstNode);
 				}
 		
-			
+					
+				//generate code for a boolean not equals expression
 				if(currentAstNode.getValue().matches("!=")){
 					System.out.println("Handling code gen for a boolean (not equals) expression");
-					return handleBoolNE(currentAstNode);
+					handleBoolNE(currentAstNode);
 				}
 		
-	
+				//generate code for a boolean equals expression
 				if(currentAstNode.getValue().matches("==")){
 					System.out.println("Handling code gen for a boolean (equals) expression");
-					return handleBoolEq(currentAstNode);
+					handleBoolEq(currentAstNode);
 				}				
 			}
 		}
-		
-		return null;
 	}
 
 
-	private String handleCharList(Node currentAstNode) {
+	/**
+	 * allocates heap storage for a string and places its reference in the accumulator
+	 * @param currentAstNode the node contatining the charlist
+	 */
+	private void handleCharList(Node currentAstNode) {
 		//add the string to the heap
 		String value= currentAstNode.getValue().replace("\"", "");
 		int sLocation = addStringToHeap(value);
@@ -280,56 +327,82 @@ public class CodeGenerator {
 		//load the accumulator with the beggining of the string in memory (in the heap)
 		addByte(OpCode.LOADACCWITHCONST.getOpcode());
 		addByte(hexRepresentation.toUpperCase());
-		
-		return null;
 	}
 
 
-	private String handleDigit(Node currentAstNode) {
+	/**
+	 * Converts the integer to a hex value and places the result in the accumulator
+	 * @param currentAstNode
+	 * @return
+	 */
+	private void handleDigit(Node currentAstNode) {
+		//load the accumulator 
 		addByte(OpCode.LOADACCWITHCONST.getOpcode());
+		
+		//build the hex representation
 		String integerAsHex = Integer.toHexString(Integer.valueOf(currentAstNode.getValue()));
 		if(integerAsHex.length() < 2){
 			integerAsHex = "0" + integerAsHex;
 		}
+		//load the byte into the accumulator
 		addByte(integerAsHex);
-		return null;
 	}
 
-
-	private String handleBoolEq(Node currentAstNode) {
+	/**
+	 * Handles a boolean equals expression
+	 * @param currentAstNode the '==' node 
+	 * 
+	 */
+	private void handleBoolEq(Node currentAstNode) {
 		// TODO Auto-generated method stub
-		return null;
 	}
 
 
-	private String handleBoolNE(Node currentAstNode) {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * Handles a boolean not equals expression
+	 * @param currentAstNode the '!=' node
+	 */
+	private void handleBoolNE(Node currentAstNode) {
+	
 	}
 
 
-	private String handleBoolVal(Node currentAstNode) {
+	/**
+	 * Loads the accumulator with a boolean value
+	 * 0 for false
+	 * 1 for true
+	 * @param currentAstNode the boolean value node
+	 * 
+	 */
+	private void handleBoolVal(Node currentAstNode) {
+		
+		//if true, load the accumulator with 1
 		if(currentAstNode.getValue().matches("true")){
 			addByte(OpCode.LOADACCWITHCONST.getOpcode());
 			addByte("01");	
 		}
 		
+		//if false, load accumulator with 0
 		if(currentAstNode.getValue().matches("false")){
 			addByte(OpCode.LOADACCWITHCONST.getOpcode());
 			addByte("00");	
 		}
-		return null;
 	}
 
-
-	private String handleAdditionOperation(Node currentAstNode) {
+	/**
+	 * Handles intops and nested intops.
+	 * @param currentAstNode the '+' node
+	 */
+	private void handleAdditionOperation(Node currentAstNode) {
+		//get the left and right operands
 		ArrayList<Node> operands = currentAstNode.getChildren();
 		Node leftOperand = operands.get(0);
 		Node rightOperand = operands.get(1);
 		
 		//load acc with whatever the right operand is
 		generateStatementCode(rightOperand);
-		//store the accumulator, going to use FF for integer operations
+		
+		//store the accumulator, going to use FF for a temp register
 		addByte(OpCode.STOREACC.getOpcode());
 		addByte(TEMPREGISTER);
 		addByte("00");
@@ -341,28 +414,42 @@ public class CodeGenerator {
 		addByte(OpCode.ADDWITHCARRY.getOpcode());
 		addByte(TEMPREGISTER);
 		addByte("00");
-		return null;
 	}
 
-
-	private String handleIdentifier(Node currentAstNode, Token token) {
+	/**
+	 * Handles identifier references
+	 * @param currentAstNode the identifier node
+	 * @return
+	 */
+	private void handleIdentifier(Node currentAstNode) {
+		//get the symbol table entry for the identifier
 		SymbolEntry idEntry  = currentAstNode.getSymbolTableData();
+		
+		//get the temp location from the static table
 		String location = staticTable.getTempLocationFromVar(currentAstNode.getValue()+"@" + idEntry.getScope());
+		
+		//load the accumulator from memory location of the variable
 		addByte(OpCode.LOADACCFROMMEM.getOpcode());
 		addByte(location);
 		addByte("XX");
-		return null;
 	}
 
-
+	
+	/**
+	 * Handles assignment statements
+	 * @param currentAstNode the '=' node
+	 */
 	private void handleAssignmentStatement(Node currentAstNode) {
 		ArrayList<Node> children = currentAstNode.getChildren();
+		
+		//get the identifier node
 		Node identifier = children.get(0);
+		
+		//get the symbol table entry for this identifier
 		SymbolEntry idEntry = identifier.getSymbolTableData();
+		
 		//get the temp location pointer from the static table
 		String tempLocation = staticTable.getTempLocationFromVar(identifier.getValue() + "@" + idEntry.getScope());
-		//get the entry from the temp location
-		StaticTable.StaticEntry tableEntry = staticTable.getEntryFromTable(tempLocation);
 		
 		//get the expression we want to evaluate and assign to the variable.
 		Node expr = children.get(1);
@@ -410,7 +497,6 @@ public class CodeGenerator {
 			if(type.matches("string")){
 				//load y register with the contents of the address for this variable
 				String temp = staticTable.getTempLocationFromVar(exprNode.getValue() + "@" + entry.getScope());
-				
 				addByte(OpCode.LOADYREGFROMMEM.getOpcode());
 				addByte(temp);
 				addByte("XX");
@@ -435,16 +521,18 @@ public class CodeGenerator {
 				hexRepresentation = "0" + hexRepresentation;
 			}
 			addByte(hexRepresentation.toUpperCase());
-			
+	
 			addByte(OpCode.LOADXREGWITHCONST.getOpcode());
 			addByte("01");
 			addByte("FF");
 		}
 		
+		//print a boolean value
 		if(exprNode.getValue().matches("true|false")){
 		
 		}
 		
+		//print a string
 		if(exprNode.getValue().matches("\"([^\"]*)\"")){
 			handleCharList(exprNode);
 			addByte(OpCode.STOREACC.getOpcode());
@@ -463,6 +551,7 @@ public class CodeGenerator {
 		
 		}
 		
+		//print an addition statement
 		if(exprNode.getValue().matches("\\+")){
 			//should perform addition, leaving the value to be printed in the accumulator
 			handleAdditionOperation(exprNode);
@@ -480,36 +569,59 @@ public class CodeGenerator {
 	
 		}
 
-	
+		
+		//print the result of a boolean not equals statement
 		if(exprNode.getValue().matches("!=")){
 	
 		}
 
-
+		
+		//print the result of a boolean equals statement
 		if(exprNode.getValue().matches("==")){
 	
 		}
 	}
 
-
+	
+	/**
+	 * Handles if statements
+	 * @param currentAstNode the if statement node
+	 */
 	private void handleIfStatement(Node currentAstNode) {
 		// TODO Auto-generated method stub
 		
 	}
 
-
+	
+	/**
+	 * Handles while statements
+	 * @param currentAstNode the while statement node
+	 */
 	private void handleWhileStatement(Node currentAstNode) {
 		// TODO Auto-generated method stub
 		
 	}
 
 
+	
+	/**
+	 * Generates code for variable declarations
+	 * @param currentAstNode the vardecl node
+	 */
 	private void handleVarDecl(Node currentAstNode) {
 		ArrayList<Node> children = currentAstNode.getChildren();
+		
+		//get the type of this identifer
 		Node type = children.get(0);
+		
+		
+		//get identifier node
 		Node identifier = children.get(1);
 		SymbolEntry idEntry = identifier.getSymbolTableData();
+		
 		switch(type.getValue()){
+		
+		//its an int
 			case("int"):{
 				//load ACC with 00, all integers are initialized to 0
 				addByte(OpCode.LOADACCWITHCONST.getOpcode());
@@ -526,6 +638,7 @@ public class CodeGenerator {
 				
 			break;
 			
+			//its a string
 			case("string"):{
 				//load ACC with 00
 				addByte(OpCode.LOADACCWITHCONST.getOpcode());
@@ -542,8 +655,10 @@ public class CodeGenerator {
 			
 			break;
 			
+			
+			//its a boolean 
 			case("boolean"):{
-				//load ACC with 00, all integers are initialized to 0
+				//load ACC with 00, all booleans are initialized to 0
 				addByte(OpCode.LOADACCWITHCONST.getOpcode());
 				addByte("00");
 				
@@ -576,6 +691,7 @@ public class CodeGenerator {
 		
 		scopeCounter += 1;
 		
+		//generate code for each statement node
 		for(Node node : currentAstNode.getChildren())
 			this.generateStatementCode(node);
 		
@@ -591,10 +707,11 @@ public class CodeGenerator {
 	private void addByte(String byteToAdd){
 		System.out.println("Adding " + byteToAdd + " to the target file" );
 		
-		//if 
+		//make sure the code table doesnt go over 256 bytes or heap storage starts overwriting op codes
 		if(currentByte>MAXPROGRAMSIZE || currentByte>= heapPointer){
 			codeOverFlow();
 		}
+		//if there isnt a problem, add the byte to the code table
 		else{
 			codeTable[currentByte] = byteToAdd;
 			currentByte++;
@@ -602,11 +719,19 @@ public class CodeGenerator {
 		
 	}
 	
+	/**
+	 * Adds a byte to the heap
+	 * @param byteToAdd te byte we want to add to the heap
+	 */
 	private void addByteToHeap(String byteToAdd){
 		System.out.println("Adding " + byteToAdd + "to the heap");
+		
+		//make sure the code table doesnt go over 256 bytes or heap storage starts overwriting op codes
+		//raise an error if so
 		if(heapPointer <= currentByte || heapPointer <= 0){
 			codeOverFlow();
 		}
+		//if there isnt a problem, add the byte to the heap
 		else{
 			codeTable[heapPointer] = byteToAdd;
 			heapPointer --;
@@ -617,6 +742,7 @@ public class CodeGenerator {
 	
 	/**
 	 * Error to raise when the program becomes to large to be executed.
+	 * Exits gracefully
 	 */
 	private void codeOverFlow(){
 		System.out.println("This program is too large. Compilation failure");
